36a66641622b52d29d00a97c3cf3e4ebc709c385
CanerTanriverdi <canertanriverdiis@gmail.com>
2025-03-09 16:43:56 +0300

    colors updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,33 +4,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        slate: {
-          700: "#2d3748",
-        },
-        primary: {
-          100: "#f0f4ff",
-          200: "#d9e4ff",
-          300: "#a9c6ff",
-          400: "#7ba7ff",
-          500: "#508bff",
-          600: "#2a69ff",
-          700: "#1e4fff",
-          800: "#1435e0",
-          900: "#0e2cc0",
-        },
-        secondary: {
-          100: "#f0f4ff",
-          200: "#d9e4ff",
-          300: "#a9c6ff",
-          400: "#7ba7ff",
-          500: "#508bff",
-          600: "#2a69ff",
-          700: "#1e4fff",
-          800: "#1435e0",
-          900: "#0e2cc0",
-        },
+        primary: "#2C2A2F", // Ana vurgu rengi (Öne çıkan butonlar, linkler vb.)
+        secondary: "#FFFFFF", // İkincil renk (Yan bileşenler, koyu alanlar)
+        accent: "#E6AD6A", // Vurgu rengi (Arka plan detayları)
+        background: "#D7D7D7", // Genel arka plan rengi
+        text: "#2C2A2F", // Genel metin rengi
       },
     },
-    plugins: [],
   },
+  plugins: [],
 };
